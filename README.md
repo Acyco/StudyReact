5dcbc453e69c734c26bf1baaa0b300b5f5c608fd
Acyco <chencongyi@live.com>
2023-12-10 19:08:00 +0800

    自定义Hook实现：自定义Hook函数

--- README.md
+++ README.md
@@ -825,6 +825,59 @@ function App() {
 ```
 
 
+## 自定义 Hook 实现
+
+### 自定义 Hook 函数
+
+概念：自定义 Hook 是以 `use 开头的函数`，通过自定义 Hook函数可以用来实现 `逻辑的封装和复用`
+
+* 不封闭
+```js
+import { useState} from "react";
+function App() {
+  const [value,setValue] = useState(true)
+  const toggle = () => setValue(!value)
+  return (
+    <div>
+      {value && <div>this is div</div>}
+      <button onClick={toggle}>toggle</button>
+    </div>
+  )
+}
+```
+* 封装自定义 
+
+```js
+// 问题： 布尔切换的逻辑 当前组件耦合在一起的 不方便复用
+// 解决思路： 自定义hook
+
+import {useEffect, useState} from "react";
+
+function useToggle () {
+  const [value, setalue] = useState(true)
+  const toggle = () => setValue(!value)
+  // 哪些状态和回调函数需要在其他组件使用 return
+  return {
+    value,
+    toggle
+  }
+}
+// 封装自定义 hook 通用思路
+// 1. 声明一个以 use 打头的函数
+// 2. 在函数体内封装可利用的逻辑（只要是可用的逻辑）
+// 3. 把组件中用到的状态或回调return出去 (以对象或数组）
+// 4. 在哪个组件中要用到这个逻辑， 就执行这个函数， 解构出来状态和回调进行使用
+
+function App() {
+  const [value,toggle] = useToggle();
+  return (
+    <div>
+      {value && <div>this is div</div>}
+      <button onClick={toggle}>toggle</button>
+    </div>
+  )
+}
+```
 
 
 
--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,32 +1,33 @@
 // 项目的根组件
 // App -> index.js -> public/index.html
 
+
+// 问题： 布尔切换的逻辑 当前组件耦合在一起的 不方便复用
+// 解决思路： 自定义hook
+
 import {useEffect, useState} from "react";
 
-function Son () {
-  // 1. 渲染时开启一个定时器
-  useEffect(() => {
-    const timer = setInterval( ()=> {
-      console.log("定时器执行中...")
-    }, 1000)
-
-    return () => {
-      // 清除副作用 （组件卸载时）
-      clearInterval(timer)
-    }
-  })
-  return (
-    <div>this is son</div>
-  )
+function useToggle () {
+  const [value, setalue] = useState(true)
+  const toggle = () => setValue(!value)
+  // 哪些状态和回调函数需要在其他组件使用 return
+  return {
+    value,
+    toggle
+  }
 }
+// 封装自定义 hook 通用思路
+// 1. 声明一个以 use 打头的函数
+// 2. 在函数体内封装可利用的逻辑（只要是可用的逻辑）
+// 3. 把组件中用到的状态或回调return出去 (以对象或数组）
+// 4. 在哪个组件中要用到这个逻辑， 就执行这个函数， 解构出来状态和回调进行使用
 
 function App() {
-  // 通过条件渲染模拟组件卸载
-  const [show,setShow] = useState(true)
+  const [value,toggle] = useToggle();
   return (
     <div>
-      {show && <Son />}
-      <button onClick={()=>setShow(false)}>卸载Son组件</button>
+      {value && <div>this is div</div>}
+      <button onClick={toggle}>toggle</button>
     </div>
   )
 }
